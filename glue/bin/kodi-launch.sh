29a0f537740495621a2319735c9454470969bb1d
Alan Griffiths <alan@octopull.co.uk>
2019-07-29 14:26:11 +0100

    Wait for Wayland socket

--- glue/bin/kodi-launch.sh
+++ glue/bin/kodi-launch.sh
@@ -4,6 +4,9 @@ set -x
 
 mkdir -p $XDG_RUNTIME_DIR -m 700
 
-while ! ln -sf $(dirname $XDG_RUNTIME_DIR)/wayland-0 $XDG_RUNTIME_DIR; do sleep 4; done
+real_wayland=$(dirname $XDG_RUNTIME_DIR)/wayland-0
+while [ ! -O ${real_wayland} ]; do echo waiting for Wayland socket; sleep 4; done
+
+ln -sf "${real_wayland}" $XDG_RUNTIME_DIR
 
 WINDOWING=wayland exec ${SNAP}/usr/bin/kodi $0
