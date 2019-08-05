ca155429b75b10f0262a99beb3f00ee31f08d345
Alan Griffiths <alan@octopull.co.uk>
2019-08-05 17:11:36 +0100

    Drop the excessive use of bash

--- glue/bin/kodi-connect.sh
+++ glue/bin/kodi-connect.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 for PLUG in alsa avahi-observe hardware-observe locale-control mount-observe network-observe removable-media shutdown system-observe wayland; do sudo snap connect mir-kiosk-kodi:${PLUG}; done
--- glue/bin/kodi-launch.sh
+++ glue/bin/kodi-launch.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -x
 
-mkdir -p $XDG_RUNTIME_DIR -m 700
+mkdir -p "$XDG_RUNTIME_DIR" -m 700
 
-real_wayland=$(dirname $XDG_RUNTIME_DIR)/wayland-0
-while [ ! -O ${real_wayland} ]; do echo waiting for Wayland socket; sleep 4; done
+real_wayland=$(dirname "$XDG_RUNTIME_DIR")/wayland-0
+while [ ! -O "${real_wayland}" ]; do echo waiting for Wayland socket; sleep 4; done
 
-ln -sf "${real_wayland}" $XDG_RUNTIME_DIR
+ln -sf "${real_wayland}" "$XDG_RUNTIME_DIR"
 
-WINDOWING=wayland exec ${SNAP}/usr/bin/kodi $0
+WINDOWING=wayland exec "${SNAP}/usr/bin/kodi" "$0"
