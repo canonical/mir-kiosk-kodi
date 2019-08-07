329022facc81d3d182a2015429e778e439e4b478
Alan Griffiths <alan@octopull.co.uk>
2019-08-07 14:23:50 +0100

    Respect WAYLAND_DISPLAY if set.

--- glue/bin/kodi-launch.sh
+++ glue/bin/kodi-launch.sh
@@ -4,7 +4,11 @@ set -x
 
 mkdir -p "$XDG_RUNTIME_DIR" -m 700
 
-real_wayland=$(dirname "$XDG_RUNTIME_DIR")/wayland-0
+if [ -z "${WAYLAND_DISPLAY}" ]
+then WAYLAND_DISPLAY=wayland-0
+fi
+
+real_wayland=$(dirname "$XDG_RUNTIME_DIR")/${WAYLAND_DISPLAY}
 while [ ! -O "${real_wayland}" ]; do echo waiting for Wayland socket; sleep 4; done
 
 ln -sf "${real_wayland}" "$XDG_RUNTIME_DIR"
