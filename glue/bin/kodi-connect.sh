19295e99492cb54511cc8c7d67d3bcf3572c2996
Alan Griffiths <alan@octopull.co.uk>
2019-09-12 14:17:02 +0100

    Use mir-kiosk-snap-launch

--- glue/bin/kodi-connect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-for PLUG in alsa avahi-observe hardware-observe locale-control mount-observe network-observe removable-media shutdown system-observe wayland; do sudo snap connect mir-kiosk-kodi:${PLUG}; done
--- glue/bin/kodi-daemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-
-if grep -q snap_core= /proc/cmdline || [ "$(snapctl get daemon)" = "true" ]
-then exec "$(dirname "$0")/kodi-launch.sh" "$@"
-fi
